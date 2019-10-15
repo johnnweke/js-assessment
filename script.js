db42bb2d51c6fdeb5e6b4e939544fa0dc0eab4e1
John Nweke <johnnweke@John-Nwekes-Mac.hsd1.md.comcast.net>
2019-10-15 03:16:45 -0400

    Did questions 1-7; stuck on 8

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 //Declare a variable called 'name' that has the value of your name.
 
 /*code here*/
-var name = ["John"]
-console.log(name)
+var name = 'John';
+console.log(name);
 
 //#2
 //create an if statement that checks to see if your name is equal to 'Ben'
 //If yes, log 'yes'
 //otherwise log 'no'
 //note: use console.log to log the string
-
 /*code here*/
+if (name === 'Ben') {
+    console.log(`Yes`);
+} else {
+    console.log(`No`);
+};
+
 
 //#3
 //create an array called 'favoriteFoods'.
@@ -19,10 +24,16 @@ console.log(name)
 
 /*code here*/
 
+let favoriteFoods = ['Rice', 'beans', 'Garri', 'Soup', 'Nkwobi']
+
 //#4
 //use a for loop to log each food in the 'favoriteFoods' array
-
 /*code here*/
+for (i = 0; i < favoriteFoods.length; i++) {
+    console.log(favoriteFoods[i]);
+};
+
+
 
 //#5
 //create an object called 'favoriteMovie'.
@@ -31,13 +42,22 @@ console.log(name)
 //give 'favoriteMovie' a property called 'director' and set it equal to the director's name
 
 /*code here*/
+let favoriteMovie = {
+    runtime: 112,
+    title: 'The GodFather',
+    director: 'Woody Allen'
+};
 
 //#6
 //create a function called 'sayHi'.
 //'sayHi' should accept one argument called 'name'
 //when 'sayHi' is invoked it should log the string 'Hello <name>!' where <name> is equal to the 'name' argument
-
 /*code here*/
+function sayHi(name) {
+    return `Hello ${name}!`;
+};
+sayHi();
+
 
 //#7
 //create an array called 'friends'
@@ -47,12 +67,40 @@ console.log(name)
 
 /*code here*/
 
+const friends = [
+    {
+        name: 'Kayode',
+        age: 23,
+        vocation: 'Programmer'
+    },
+    {
+        name: 'Pastor Bunmi',
+        age: 49,
+        vocation: 'Pastor'
+
+    },
+    {   
+        name: 'Deacon Anthony',
+        age: 62,
+        vocation: 'Deacon & Train Operator'
+
+    }
+];
+
 //#8
 //use a for loop to iterate over the 'friends' array from problem #7
 //inside the for loop print the string 'My friend <name> is <age> years old and does <vocation> for work.'
 
 /*code here*/
-
+printFriends() {
+for (i = 0; i < friends; i++) {
+    // let friendsName = friends[i].name;
+    // let friendsAge = friends[i].age;
+    // let friendsVocation = friends[i].vocation;
+    console.log ( `My friend ${friends[i].nameName} is ${friends[i].age} and does ${friends[i].vocation} for work.`);
+};
+};
+printFriends();
 //#9
 //create a constructor called 'User' that can function as a class for creating new user objects
 //'User' should take 'email', and 'password' as arguments 
